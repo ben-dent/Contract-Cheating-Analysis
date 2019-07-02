dc6d6acc57c6994b8484dc0492bbdfe0d2d7419c
Ben Dent <bjd18@ic.ac.uk>
2019-07-02 16:05:17 +0100

    Updating some outputs

--- Main.py
+++ Main.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3 as lite
 import sys
-from PyQt4 import QtCore, QtGui, uic
+from PyQt5 import uic, QtWidgets
+
 
 mainUI = uic.loadUiType("UIs/main.ui")[0]
 
-class Main(QtGui.QMainWindow, mainUI):
+
+class Main(QtWidgets.QMainWindow, mainUI):
     def __init__(self, parent = None):
-        QtGui.QMainWindow.__init__(self, parent)
+        QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.btnFetch.clicked.connect(self.fetch)
         self.btnExit.clicked.connect(self.exit)
@@ -20,10 +22,10 @@ class Main(QtGui.QMainWindow, mainUI):
         valid = False
         url = self.edtURL.text()
 
-        # Checking if the user has entered anythig in the text box
+        # Checking if the user has entered anything in the text box
         if (url == ""):
-            QtGui.QMessageBox.warning(self, "Invalid Entry", "Please enter a URL!", QtGui.QMessageBox.Ok,
-                                      QtGui.QMessageBox.Ok)
+            QtWidgets.QMessageBox.warning(self, "Invalid Entry", "Please enter a URL!", QtWidgets.QMessageBox.Ok,
+                                      QtWidgets.QMessageBox.Ok)
             self.edtURL.setFocus()
         else:
             valid = True
@@ -34,22 +36,34 @@ class Main(QtGui.QMainWindow, mainUI):
             # Checking if the user entered a valid URL
             try:
                 r = requests.get(url)
-
                 soup = BeautifulSoup(r.content, "html.parser")
 
                 # Finding the average that freelancers are bidding - the first h2 HTML tag
-                a = soup.find("h2")
-                division = a.text.split(" ")
+                data1 = soup.find("h2")
 
-                numFreelancers = division[0] + " " + division[1]
-                averagePrice = "Bidding an average of " + division[6]
-                self.lblFreelancers.setText(numFreelancers)
-                self.lblAvPrice.setText(averagePrice)
+                # Check if anyone has bid on the job yet
+                if (data1.text.split("Need to")[0] == data1.text):
+                    division = data1.text.split(" ")
 
+                    numFreelancers = division[0] + " " + division[1]
+                    averagePrice = "Bidding an average of " + division[6]
+                    self.lblFreelancers.setText(numFreelancers)
+                    self.lblAvPrice.setText(averagePrice)
+                else:
+                    self.lblFreelancers.setText("Nobody has bid on this yet")
+                    self.lblAvPrice.setText("No bids yet")
+
+                data2 = soup.find_all("span")
+                for item in data2:
+                    if (item.get("itemprop") == "addressLocality"):
+                        b = item.text.split(", ")[1]
+                        country = b.split("\n")[0]
+                        self.lblCountry.setText(country)
+                        break
 
             except requests.exceptions.MissingSchema as e:
-                QtGui.QMessageBox.warning(self, "Invalid entry", "Please enter a valid URL!", QtGui.QMessageBox.Ok,
-                                          QtGui.QMessageBox.Ok)
+                QtWidgets.QMessageBox.warning(self, "Invalid entry", "Please enter a valid URL!",
+                                              QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
                 self.edtURL.setText("")
                 self.edtURL.setFocus()
 
@@ -59,7 +73,7 @@ class Main(QtGui.QMainWindow, mainUI):
             self.fetch()
 
 
-app = QtGui.QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 main = Main()
 main.show()
 app.exec()
