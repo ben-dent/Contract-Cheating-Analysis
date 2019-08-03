7ee04d90d68df168f869c6e5e7092cbb7688f998
Ben Dent <bjd18@ic.ac.uk>
2019-08-03 15:41:32 +0100

    Theme

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 # html_theme = 'default'
-html_theme = 'alabaster'
+html_theme = 'rtd-dropdown'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
