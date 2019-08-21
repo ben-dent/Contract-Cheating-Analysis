4ca64604e35f100e597262b80cb0d8881b47021a
Ben Dent <bjd18@ic.ac.uk>
2019-08-21 11:41:14 +0100

    Again

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -604,7 +604,7 @@ def jobConversions():
 def reviewJobConversions():
     con = lite.connect(DATABASE_NAME)
     cur = con.cursor()
-    cur.execute("SELECT JobID, FinalCost, Currency, TimeAgo, Week FROM Jobs WHERE ConvertedFinalCost = 'None'")
+    cur.execute("SELECT JobID, FinalCost, Currency, TimeAgo, Week FROM ReviewJobs WHERE ConvertedFinalCost = 'None'")
 
     res = cur.fetchall()
 
